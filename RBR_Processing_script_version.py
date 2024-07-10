6ca07330fd6e2ceea3b9c8c209219c558cdff32d
hhourston <hana.hourston@dfo-mpo.gc.ca>
2024-07-10 15:10:40 -0700

    update to use new version of write_header()

--- RBR_Processing_script_version.py
+++ RBR_Processing_script_version.py
@@ -7,7 +7,8 @@ from RBR_CTD_IOS_2024 import *
 year = "2024"
 cruise_number = "017"
 # dest_dir = f"C:\\Users\\huntingtons\\Desktop\\RBR_Processing\\CURRENT_PROCESSING\\{year}-{cruise_number}\\Suggested\\"
-dest_dir = "C:\\Users\\huntingtons\\\Desktop\\RBR_Processing\\Github_June2024_test\\"
+# dest_dir = "C:\\Users\\huntingtons\\Desktop\\RBR_Processing\\Github_June2024_test\\"
+dest_dir = 'C:\\Users\\hourstonh\\Documents\\ctd_processing\\RBR\\2024-017\\'
 skipcasts = 0
 rsk_start_end_times_file = None
 rsk_time1, rsk_time2 = [None, None]
@@ -20,7 +21,8 @@ shift_recs_conductivity: int = 2
 shift_recs_oxygen = -11
 drop_vars_file = "2024_017_vars_to_drop.csv"
 processing_report_name = f"{year}-{cruise_number}_RBR_Processing_Report.docx"
-# 5 lines of processing comments are added here and included in the write_comments function.  If more lines are needed, adjust the function.
+# 5 lines of processing comments are added here and included in the write_comments function.
+# If more lines are needed, adjust the function.
 processing_comments1 = "       " + "Cast 18 stopped at 20m and was brought to the surface and re-started."
 processing_comments2 = None  # format is "       " + "Your text here"
 processing_comments3 = None
@@ -41,9 +43,10 @@ fill_type = None
 spk_window = None
 spk_std = None
 spk_var = None
+
 READ_RSK(dest_dir, year, cruise_number,
-         skipcasts, meta_dict, zoh, fill_action,
-         fill_type, spk_window, spk_std, spk_var, fix_spk,
+         skipcasts, meta_dict, zoh, fix_spk, fill_action,
+         fill_type, spk_window, spk_std, spk_var,
          rsk_start_end_times_file, rsk_time1, rsk_time2)
 # first steps are just in a cell in the notebook, not wrapped in a function
 
@@ -60,6 +63,8 @@ check_profiles(dest_dir, year, cruise_number, name1="Pre_Processing_", name2="Pr
 
 # Make the first corrections
 
+dict_recs_out = {}
+
 fill_action = "interp"
 fill_type = "interpolated value"
 spk_window = 11
@@ -67,15 +72,21 @@ spk_std = 3
 spk_var = "Fluorescence:URU"
 input_ext = first_corrections(dest_dir, year, cruise_number,
                               skipcasts, meta_dict, verbose,
-                              rsk_file, fill_action, fill_type,
+                              rsk_file,
+                              rsk_start_end_times_file, rsk_time1, rsk_time2,
+                              fill_action, fill_type,
                               spk_window, spk_std, spk_var,
-                              rsk_start_end_times_file, rsk_time1, rsk_time2)
+                              )
 
 # Create cast variables and make first plots
 
 cast, cast_d, cast_u = CREATE_CAST_VARIABLES(
     year, cruise_number, dest_dir, input_ext
 )
+
+# If zoh applied in first_corrections()
+dict_recs_out['ZEROORDER'] = add_recs_out(cast_d)
+
 for cast_i in cast_d.keys():
     have_oxy = True if "Oxygen" in cast_d[cast_i].columns else False
     have_fluor = True if "Fluorescence" in cast_d[cast_i].columns else False
@@ -84,6 +95,7 @@ for cast_i in cast_d.keys():
 
     print(f"have_oxy: {have_oxy}, have_fluor: {have_fluor}")
     break
+
 first_plots(year, cruise_number, dest_dir, input_ext)
 
 # Start data processing, correct pressure and time if needed, clip casts
@@ -94,18 +106,23 @@ limit_pressure_change = 0.02
 # correct time
 if start_time_correction_file is not None:
     cast_correct_t, cast_d_correct_t, cast_u_correct_t = CORRECT_TIME_OFFSET(
-        dest_dir, cast, cast_d, cast_u, metadata_dict, start_time_correction_file
+        dest_dir, cast, cast_d, cast_u, meta_dict, start_time_correction_file
     )
+    dict_recs_out['CORRECT_TIME_OFFSET'] = add_recs_out(cast_d_correct_t)
+
     if verbose:
         print("Time offset(s) corrected")
 else:
     cast_correct_t, cast_d_correct_t, cast_u_correct_t = cast, cast_d, cast_u
+
 # correct pressure depth
 if pd_correction_value != 0:
     cast_pc, cast_d_pc, cast_u_pc = CALIB(
         cast_correct_t, cast_d_correct_t, cast_u_correct_t, meta_dict,
         pd_correction_value
     )  # 0 if no neg pressures
+    dict_recs_out['CALIB'] = add_recs_out(cast_d_pc)
+
     if verbose:
         print(
             "The following correction value has been applied to Pressure and Depth:",
@@ -123,6 +140,8 @@ cast_u_clip = CLIP_CAST(
     cast_u_pc, meta_dict, limit_pressure_change=-0.02, cast_direction="up"
 )
 
+dict_recs_out['CLIP'] = add_recs_out(cast_d_clip)
+
 plot_clip(cast_d_clip, cast_d_pc, dest_dir)
 
 if verbose:
@@ -131,12 +150,13 @@ if verbose:
 # Filter and Shift the data, confirm parameters here, convert oxygen
 
 filter_type = 1  # Moving average, 0 for FIR
-sample_rate = 8  # from rsk file, could be 6Hz
+# sample_rate = 8  # from rsk file, could be 6Hz
 time_constant = 1 / 8
 window_width = 3
 
 # Filter
 sample_rate = int(np.round(1 / float(meta_dict["Sampling_Interval"])))
+
 cast_d_filtered, cast_u_filtered = FILTER(
     cast_d_clip,
     cast_u_clip,
@@ -153,10 +173,13 @@ plot_filter(
     cast_d_filtered, cast_u_filtered, cast_d_clip, cast_u_clip, dest_dir, have_fluor
 )
 
+dict_recs_out['FILTER'] = add_recs_out(cast_d_filtered)
+
 if verbose:
     print(
         f"Casts filtered, assuming a sample rate of {sample_rate} records per second"
     )
+
 # Shift C
 cast_d_shift_c, cast_u_shift_c = SHIFT_CONDUCTIVITY(
     cast_d_filtered,
@@ -164,6 +187,8 @@ cast_d_shift_c, cast_u_shift_c = SHIFT_CONDUCTIVITY(
     metadata_dict=meta_dict,
     shifted_scan_number=shift_recs_conductivity,
 )
+
+dict_recs_out['SHIFT_Conductivity'] = add_recs_out(cast_d_shift_c)
 
 plot_shift_c(
     cast_d_shift_c, cast_u_shift_c, cast_d_filtered, cast_u_filtered, dest_dir
@@ -175,13 +200,14 @@ if verbose:
 # Shift O
 
 if have_oxy:
-
     cast_d_shift_o, cast_u_shift_o = SHIFT_OXYGEN(
         cast_d_shift_c,
         cast_u_shift_c,
         metadata_dict=meta_dict,
         shifted_scan_number=shift_recs_oxygen,
     )
+
+    dict_recs_out['SHIFT_Oxygen'] = add_recs_out(cast_d_shift_o)
 
     plot_shift_o(
         cast_d_shift_o, cast_u_shift_o, cast_d_shift_c, cast_u_shift_c, dest_dir
@@ -194,6 +220,8 @@ if have_oxy:
     cast_d_o_conc, cast_u_o_conc = DERIVE_OXYGEN_CONCENTRATION(
         cast_d_shift_o, cast_u_shift_o, meta_dict
     )
+
+    dict_recs_out['DERIVE_OXYGEN_CONCENTRATION'] = add_recs_out(cast_d_o_conc)
 
     if verbose:
         print("Oxygen concentration derived from oxygen saturation")
@@ -209,6 +237,8 @@ cast_d_wakeeffect, cast_u_wakeeffect = DELETE_PRESSURE_REVERSAL(
     cast_d_o_conc, cast_u_o_conc, metadata_dict=meta_dict
 )
 
+dict_recs_out['DELETE_PRESSURE_REVERSAL'] = add_recs_out(cast_d_wakeeffect)
+
 if verbose:
     print("Deleted pressure change reversals")
 
@@ -220,6 +250,8 @@ if drop_vars_file is not None:
     cast_d_dropvars = DROP_SELECT_VARS(
         dest_dir, cast_d_wakeeffect, drop_vars_file, meta_dict
     )
+    dict_recs_out['DROP_SELECT_VARS'] = add_recs_out(cast_d_dropvars)
+
     if verbose:
         print("Select variables dropped from casts specified in", drop_vars_file)
 else:
@@ -227,9 +259,11 @@ else:
 
 # Binave
 
-cast_d_binned = BINAVE(
-    cast_d_dropvars, metadata_dict=meta_dict
-)
+cast_d_dropvars.pop('cast918')
+
+cast_d_binned = BINAVE(cast_d_dropvars, metadata_dict=meta_dict)
+
+dict_recs_out['BINAVE'] = add_recs_out(cast_d_binned)
 
 figure_dir = os.path.join(dest_dir, "FIG")
 if not os.path.exists(figure_dir):
@@ -246,6 +280,9 @@ if verbose:
 # Final edits and plots
 
 cast_d_final = FINAL_EDIT(cast_d_binned, meta_dict)
+
+dict_recs_out['FINALEDIT'] = add_recs_out(cast_d_final)
+
 if verbose:
     print("Final edit completed")
 
@@ -254,35 +291,42 @@ plot_processed(cast_d_final, dest_dir)
 # Write the files
 
 cast_numbers = list(map(lambda x: int(x.split("cast")[-1]), cast_d_final.keys()))
+
 for i in cast_numbers:
     # # Update have_fluor and have_oxy flags, since it could be different for different casts
     # have_fluor = True if "Fluorescence" in cast_d_final[f"cast{i}"].columns else False
     # have_oxy = True if "Oxygen" in cast_d_final[f"cast{i}"].columns else False
     channel_names = cast_d_final[f"cast{i}"].columns
+
     # print(channel_names)
     # Call the main header creation function
-    main_header(
-        dest_dir,
-        i,
-        meta_dict,
-        cast,
-        cast_d,
-        cast_d_correct_t,
-        cast_d_pc,
-        cast_d_clip,
-        cast_d_filtered,
-        cast_d_shift_c,
-        cast_d_shift_o,
-        cast_d_o_conc,
-        cast_d_wakeeffect,
-        cast_d_binned,
-        cast_d_dropvars,
-        cast_d_final,
-        channel_names,
-        processing_report_name,
-        processing_comments1,
-        processing_comments2,
-        processing_comments3,
-        processing_comments4,
-        processing_comments5
+    # main_header(
+    #     dest_dir,
+    #     i,
+    #     meta_dict,
+    #     cast,
+    #     cast_d,
+    #     cast_d_correct_t,
+    #     cast_d_pc,
+    #     cast_d_clip,
+    #     cast_d_filtered,
+    #     cast_d_shift_c,
+    #     cast_d_shift_o,
+    #     cast_d_o_conc,
+    #     cast_d_wakeeffect,
+    #     cast_d_binned,
+    #     cast_d_dropvars,
+    #     cast_d_final,
+    #     channel_names,
+    #     processing_report_name,
+    #     processing_comments1,
+    #     processing_comments2,
+    #     processing_comments3,
+    #     processing_comments4,
+    #     processing_comments5
+    # )
+
+    main_header2(
+        dest_dir, i, meta_dict, cast, cast_d_final, dict_recs_out, channel_names,
+        processing_report_name, processing_comments1
     )
